e6f4159f86c1a283d720d273778fb326d817ced6
AxelTLarsson <mail@axellarsson.nu>
2015-02-14 20:21:57 +0100

    Prep. ass. 3 done !

--- mylogin.c
+++ mylogin.c
@@ -51,92 +51,56 @@ void read_username(char *username)
 
 int main(int argc, char **argv)
 {
-  /* 
-   * Write "login: " and read user input. Copies the username to the
-   * username variable.
-   */
-  char username[USERNAME_SIZE];
-  read_username(username);
-
-  /* Write "Password: " and read user password input without echoing it. */
-  char *entered_pw[PASSWORD_SIZE];
-  *(entered_pw) = getpass("Password: "); // read the password safely
-
-  printf("\nThe password typed was: \"%s\"\n", *entered_pw);
-
-  /*
-   * Check username and password
-   */
-  struct pwdb_passwd *pwentry = pwdb_getpwnam(username);
-
-  if (pwentry != NULL)
+  while (1)
   {
+    /* 
+     * Write "login: " and read user input. Copies the username to the
+     * username variable.
+     */
+    char username[USERNAME_SIZE];
+    read_username(username);
 
-    // Retrieve the salt (by copying so that we do not lose information)
-    // Test copying strings
-    char *source[128], buf[128];
-    *(source) = "hejsan svejsan";
-    strlcpy(buf, source[0], sizeof(buf));
-    printf("source: %s\n", *source);
-    printf("buf: %s\n", buf);
+    /* Write "Password: " and read user password input without echoing it. */
+    char *entered_pw[PASSWORD_SIZE];
+    *(entered_pw) = getpass("Password: "); // read the password safely
 
-    source[0] = "Bajs";
-    printf("source: %s\n", *source);
-    printf("buf: %s\n", buf);
+    // printf("\nThe password typed was: \"%s\"\n", *entered_pw);
 
-    char salt[3];
-    strlcpy(salt, pwentry->pw_passwd, 3);
-    printf("salt: %s\n", salt);
+    /*
+     * Check username and password
+     */
+    struct pwdb_passwd *pwentry = pwdb_getpwnam(username);
 
+    if (pwentry != NULL)
+    {
 
-    // Hash entered password (overwrite memory where entered password resides with the hash instead)
+      // Retrieve the salt (by copying so that we do not lose information)
+      char salt[3];
+      strlcpy(salt, pwentry->pw_passwd, 3);
+      // printf("salt: %s\n", salt);
 
-    // Compare
+      // Hash entered password
+      char *entered_hash[PASSWORD_SIZE];
+      *(entered_hash) = crypt(*entered_pw, salt);
+      // printf("entered_hash: %s\n", *entered_hash);
 
-    // Print some info
-    printf("pwentry->pw_uid: %d\n", pwentry->pw_uid);
-    printf("pwentry->pw_passwd: %s\n", pwentry->pw_passwd);
-
-
-    char *stored_hash[PASSWORD_SIZE];
-    *(stored_hash) = pwentry->pw_passwd;
-    printf("TEST: %s\n", *stored_hash);
-
-/*
-    // Save the stored hashed password for later comparison
-    char h[PASSWORD_SIZE];
-    char *hashed_passwd[PASSWORD_SIZE];
-    *hashed_passwd[0] = pwentry->pw_passwd;
-    printf("hashed_passwd: %s\n", hashed_passwd); 
-  
-
+      // Retrieve stored hash
+      char *stored_hash[PASSWORD_SIZE];
+      *(stored_hash) = pwentry->pw_passwd;
+      // printf("stored_hash: %s\n", *stored_hash); 
     
-    // Get the salt
-    char salt[2];
-    char *psalt;
-    psalt = pwentry->pw_passwd;
-    psalt[2] = '\0';
-    printf("salt %s\n", psalt);
+      // Compare stored hash with entered password hash
+      // printf("comparing '%s' with '%s'\n", *entered_hash, *stored_hash);
+      if (strcmp(*entered_hash, *stored_hash) == 0)
+      {
+        printf("User authenticated succesfully.\n");
+        return 0;
+      }
 
-    // hash the entered password
-    char hash[PASSWORD_SIZE];
-    char *phash[PASSWORD_SIZE];
-    phash[0] = crypt(*pw, psalt);
+    } 
 
-    printf("hash: %s\n", *phash);
-    
-    
-    printf("comparing '%s' with '%s'\n", *phash, *hashed_passwd);
-    int result = strcmp(*phash, psalt);
-    printf("result: %i\n", result);
-
-    */
-
-  } else
-  {
     printf("Unknown user or incorrect password.\n");
+    
   }
-
-  return 0;
 }
   
